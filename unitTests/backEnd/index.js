514432e73d624f2fa9a0c1d427b7d3771bf46fb7
SabriSnipy <sabri.snipy@gmail.com>
2020-02-06 18:02:48 +0100

    config circle ci

---
+++ unitTests/backEnd/index.js
@@ -0,0 +1,11 @@
+process.env.NODE_ENV = 'test';
+let user=require('./user');
+let reclamation= require ('./reclamation');
+
+
+describe('USER_API', ()=>{
+    // user()
+    // reclamation()
+
+});
+
